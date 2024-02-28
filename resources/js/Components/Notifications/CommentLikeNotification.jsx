2dd10ad9e19f1e656d8edea2bc1f056d8da02068
alexanderOpenko <openkooleksandr@gmail.com>
2024-02-28 20:13:03 +0200

    Update notifications styles

--- resources/js/Components/Notifications/CommentLikeNotification.jsx
+++ resources/js/Components/Notifications/CommentLikeNotification.jsx
@@ -3,7 +3,7 @@ import { usePage } from "@inertiajs/react"
 export default function CommentLikeNotification({ userName, text, imagePath = null, birhday = null, likerId = null }) {
     const { public_url, auth } = usePage().props
 
-    return <><div className="flex justify-between">
+    return <>{auth.user.id !== likerId && <div className="flex justify-between">
         <div className="mr-4 max-w-[76%]">
             <div className="line-clamp-2 overflow-hidden">
                 <div className="font-semibold"> {userName} </div>
@@ -19,10 +19,10 @@ export default function CommentLikeNotification({ userName, text, imagePath = nu
         </div>
 
         {imagePath && <img src={public_url + "/" + imagePath} className="h-[70px] max-w-[70px] w-full object-cover h-full" />}
-    </div>
+    </div>}
 
-        {auth.user.id === likerId && <div className="flex mt-4">
-            <div className="mr-2 font-medium"> фууу самолайк </div>
+        {auth.user.id === likerId && <div className="flex justify-between mt-4">
+            <div className="font-medium"> фууу самолайк </div>
 
             <img src={public_url + "/" + "selflike.jpg"} className="h-[80px] max-w-[117px] w-full object-cover h-full" />
         </div>}
